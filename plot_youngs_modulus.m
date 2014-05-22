88925ccca9fc27db497c87168bf8df0038d18ddc
Andrew Walker <a.walker@leeds.ac.uk>
2014-05-22 12:48:49 +0100

    Errors in plot_youngs_modulis

--- plot_youngs_modulus.m
+++ plot_youngs_modulus.m
@@ -9,13 +9,14 @@
 function plot_youngs_modulus(file)
 
     % Get the data from strain fitting
-    [p, t, f, s, ~] = read_sine_fit(file);
+    [p, t, f, s, ~, s_se, ~] = read_sine_fit(file);
 
     
     % Just work on 27 ton data for now.
     temperature = t(f==27);
     period = p(f==27);
     norm_compl = s(f==27);
+    norm_compl_se = s_se(f==27);
     
     % Temperature limits
     t_max = max(temperature);
@@ -35,17 +36,21 @@ function plot_youngs_modulus(file)
     % Calculate E at period and temperature from known Al2O3 E
     n = length(norm_compl);
     E_calc = zeros(1,n);
+    E_calc_se = zeros(1,n);
     for i = 1:n
         [~, Eiso_Al2O3] = C_al2o3(2.5, (temperature(i)+273.15));
-        E_calc(i) = Eiso_Al2O3 / norm_compl(i);  
+        E_calc(i) = (Eiso_Al2O3 / norm_compl(i));
+        inv_norm_compl_se = sqrt((1/norm_compl(i))^2 * (norm_compl_se(i)/norm_compl(i)^2));
+        E_calc_se(i) = sqrt(Eiso_Al2O3^2 * inv_norm_compl_se^2);
     end
     
     % Plot something useful...
-    plot(Ts, Eiso_Zn, '-k', Ts, Emax_Zn, '--k', Ts, Emin_Zn, '--k', ... 
-        temperature(period==10), E_calc(period==10), '.k', ...
-        temperature(period==30), E_calc(period==30), '.b', ...
-        temperature(period==100), E_calc(period==100), '.g', ...
-        temperature(period==300), E_calc(period==300), '.r');
+    plot(Ts, Eiso_Zn, '-k', Ts, Emax_Zn, '--k', Ts, Emin_Zn, '--k');
+    hold on
+    errorbar(temperature(period==10), E_calc(period==10), E_calc_se(period==10), 'ok')
+    errorbar(temperature(period==30), E_calc(period==30), E_calc_se(period==30), 'ob');
+    errorbar(temperature(period==100), E_calc(period==100), E_calc_se(period==100), 'og');
+    errorbar(temperature(period==300), E_calc(period==300), E_calc_se(period==300), 'or');
     
 
 end
