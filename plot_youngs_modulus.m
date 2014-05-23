2f105027311ccd7d854a698a386642b690c5c5d2
Andrew Walker <a.walker@leeds.ac.uk>
2014-05-23 14:17:26 +0100

    Permit:
    plot_youngs_modulus('SineFits.txt', 'force', 60)
    
    of 27... to deal with the rest of the results for Zn02

--- plot_youngs_modulus.m
+++ plot_youngs_modulus.m
@@ -6,17 +6,40 @@
 %
 % Andrew Walker 20/3/2014.
 
-function plot_youngs_modulus(file)
+function plot_youngs_modulus(file, varargin)
 
+    % Defaults
+    force = 27;
+    
+    % Process the optional arguments overiding defaults
+    iarg = 1 ;
+    while iarg <= (length(varargin))
+        switch lower(varargin{iarg})
+            case 'force'
+                force = varargin{iarg+1};
+                iarg = iarg + 2;
+            otherwise
+                error(['Unknown option: ' varargin{iarg}]) ;
+        end
+    end
+
+    if force==27
+        pressure = 2.5; % ish
+    elseif force==60 
+        pressure = 5.0; %Say
+    else
+        pressure = NaN;
+    end
+    
     % Get the data from strain fitting
     [p, t, f, s, ~, s_se, ~] = read_sine_fit(file);
 
     
     % Just work on 27 ton data for now.
-    temperature = t(f==27);
-    period = p(f==27);
-    norm_compl = s(f==27);
-    norm_compl_se = s_se(f==27);
+    temperature = t(f==force);
+    period = p(f==force);
+    norm_compl = s(f==force);
+    norm_compl_se = s_se(f==force);
     
     % Temperature limits
     t_max = max(temperature);
@@ -29,7 +52,7 @@ function plot_youngs_modulus(file)
     Emax_Zn = zeros(1,n);
     Emin_Zn = zeros(1,n);
     for i = 1:n
-        [C_Zn, Eiso_Zn(i)] = C_zn(2.5, (Ts(i)+273.15));
+        [C_Zn, Eiso_Zn(i)] = C_zn(pressure, (Ts(i)+273.15));
         [Emax_Zn(i), Emin_Zn(i)] = hex_youngs_mod_lims(C_Zn);
     end
     
@@ -38,7 +61,7 @@ function plot_youngs_modulus(file)
     E_calc = zeros(1,n);
     E_calc_se = zeros(1,n);
     for i = 1:n
-        [~, Eiso_Al2O3] = C_al2o3(2.5, (temperature(i)+273.15));
+        [~, Eiso_Al2O3] = C_al2o3(pressure, (temperature(i)+273.15));
         E_calc(i) = (Eiso_Al2O3 / norm_compl(i));
         inv_norm_compl_se = sqrt((1/norm_compl(i))^2 * (norm_compl_se(i)/norm_compl(i)^2));
         E_calc_se(i) = sqrt(Eiso_Al2O3^2 * inv_norm_compl_se^2);
